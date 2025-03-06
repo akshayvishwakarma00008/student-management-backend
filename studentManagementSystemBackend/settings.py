3c00bff9b0f1e5ee10136627e3408e4ec38a840f
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2025-03-06 16:17:26 +0530

    updated cors

--- studentManagementSystemBackend/settings.py
+++ studentManagementSystemBackend/settings.py
@@ -135,6 +135,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:5173",
+    "https://student-management-ui-ashen.vercel.app",
 ]
 
 from datetime import timedelta
