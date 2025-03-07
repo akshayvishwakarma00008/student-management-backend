9be29621307c0516035e8efabc29c9136923af79
akshay-at-bluepineapple <akshay.vishwakarma@bluepineapple.io>
2025-03-07 11:53:04 +0530

    modified comand file task

--- students/management/commands/task.py
+++ students/management/commands/task.py
@@ -1,8 +1,14 @@
 from django.core.management.base import BaseCommand
 import kronos
+from account.utils import Util
 
 
 @kronos.register("* * * * *")
 class Command(BaseCommand):
     def handle(self, *args, **options):
-        print("hello world")
+        data = {
+            "subject": "Testing Cron",
+            "body": "mail getting sent",
+            "to_email": "akshayvishwakarma00008@gmail.com",
+        }
+        Util.send_email(data)
